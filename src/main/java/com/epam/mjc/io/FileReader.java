716512b3d87e046be64fdcc2d101221f0ed4169f
hydroponka <Ceageenk02@gmail.com>
2023-04-24 17:31:30 +0300

    1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,27 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.math.BigDecimal;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        String name;
+        String age;
+        String email;
+        String phone;
+        try (BufferedReader bf = new BufferedReader(new java.io.FileReader(file))) {
+            name = bf.readLine().split(": ")[1];
+            age = bf.readLine().split(": ")[1];
+            email = bf.readLine().split(": ")[1];
+            phone = bf.readLine().split(": ")[1];
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new Profile(name, Integer.parseInt(age), email, Long.parseLong(phone));
     }
 }
